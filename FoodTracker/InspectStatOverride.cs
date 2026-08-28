8abeb7d758d7bb4c0135e73a7ba30be821c90a61
Boarlock <derick.clark.jr1998@gmail.com>
2026-08-27 22:53:38 -0400

    Display remaining nutrition for partial meals

---
+++ FoodTracker/InspectStatOverride.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using Verse;
+using RimWorld;
+
+namespace FoodTracker
+{
+    [HarmonyPatch(typeof(StatWorker), nameof(StatWorker.GetValue), new[] { typeof(Thing), typeof(bool), typeof(int) }
+)]
+    public static class NutritionStatPatch
+    {
+        static bool Prefix(StatWorker __instance, Thing thing, ref float __result, StatDef ___stat)
+        {
+            if (___stat != StatDefOf.Nutrition)
+                return true;
+
+            if (thing == null || thing.Destroyed)
+                return true;
+
+            if (!FoodTrackingHelpers.IsTracked(thing))
+                return true;
+
+            CompPartialNutrition comp = thing.TryGetComp<CompPartialNutrition>();
+
+            if (comp == null)
+                return true;
+
+            __result = comp.RemainingNutrition;
+            return false;
+        }
+    }
+}
